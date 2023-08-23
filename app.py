fb606edbc8fc632a3aaf5dfe0b0a22fe3f59c9d2
carlos-romanelli <emaildoroma@gmail.com>
2023-08-23 14:18:42 -0300

    Atualização em 23/08/2023 v2

--- app.py
+++ app.py
@@ -7,31 +7,42 @@ import datetime
 
 import pyodbc
 
-
 # String de conexão com o banco de dados
 conn = pyodbc.connect('Driver={SQL Server};'
-'Server=ATI06\CESADW;'
-'Database=Redegeo-ant;'
-'UID=redegeo.cons;'
-'PWD=redegeo')
+                      'Server=ATI06\CESADW;'
+                      'Database=Redegeo-ant;'
+                      'UID=redegeo.cons;'
+                      'PWD=redegeo')
 
 # Criar um objeto cursor
 
 cursor = conn.cursor()
 
-
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', titulo='Consulta do Redegeo')
 
+
 @app.route('/atendimento')
 def atendimento():
+    return render_template('atendimento.html', titulo='Consulta atendimentos do Redegeo')
+
+@app.route('/consulta_atendimento', methods=['POST',])
+def consulta_atendimento():
     # Comando SQL a executar
-    cursor.execute('SELECT top(1000) * FROM tblAtendimentos order by id_Atend desc')
-    return render_template('atendimento.html', titulo='Consulta do Redegeo', atendimentos=cursor)
+    cursor.execute("SELECT top(1000) id_Atend as Atendimento, dcr_serv as Serviço, mat_atend as Matrícula, nm_atend as Cliente, tel_cntto_atend as Contato, dt_hr_inic_atend as [Início do atendimento], u.nome_User as Atendente, obs_atend as Observação FROM tblAtendimentos a inner join tblUsuarios u on a.cod_func_atend = u.cod_User inner join tblServicos s on a.cod_serv_sol_atend = s.cod_serv where mat_atend = '" + request.form['matricula'] + "' order by dt_hr_inic_atend desc")
+
+    # cursor.execute("SELECT top(1000) * FROM tblAtendimentos where mat_atend = '" + request.form[
+    #    'matricula'] + "' order by id_Atend desc")
+    return render_template('atendimento.html', titulo='Consulta atendimentos do Redegeo', atendimentos=cursor)
+
+
+
+
+
 
 
 # from views import *
