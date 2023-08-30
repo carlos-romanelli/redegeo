3dc2ecfcb40918d6390fc97d9bfdce473f71db46
carlos-romanelli <emaildoroma@gmail.com>
2023-08-30 10:13:45 -0300

    Atualização 30/08/2023

--- app.py
+++ app.py
@@ -35,15 +35,15 @@ def atendimento():
 def consulta_atendimento():
     # Comando SQL a executar
     cursor.execute("SELECT id_Atend as Atendimento,"
-                   " dcr_serv as Serviço, "
+                   "dcr_serv as Serviço, "
                    "mat_atend as Matrícula, "
                    "nm_atend as Cliente, "
                    "tel_cntto_atend as Contato, "
                    "dt_hr_inic_atend as [Início do atendimento], "
                    "u.nome_User as Atendente, "
                    "obs_atend as Observação FROM tblAtendimentos a "
-                   "inner join tblUsuarios u on a.cod_func_atend = u.cod_User "
-                   "inner join tblServicos s on a.cod_serv_sol_atend = s.cod_serv "
+                   "left join tblUsuarios u on a.cod_func_atend = u.cod_User "
+                   "left join tblServicos s on a.cod_serv_sol_atend = s.cod_serv "
                    "where mat_atend = '" + request.form['matricula'] + "' order by dt_hr_inic_atend desc")
     return render_template('atendimento.html', titulo='Consulta atendimentos do Redegeo', atendimentos=cursor)
 
@@ -54,19 +54,30 @@ def servico():
 @app.route('/consulta_servico', methods=['POST',])
 def consulta_servico():
     # Comando SQL a executar
-    if request.form['matricula']:
-        cursor.execute("SELECT id_ss,"
-                       "dt_hr_ss,"
-                       "dcr_tp_serv,"
+    if request.form['servico']:
+        cursor.execute("SELECT id_ss, "
+                       "format(dt_hr_ss, 'dd/MM/yyyy','en-US'), "
                        "dcr_serv, "
                        "matricula_ss, "
                        "nome_solic_ss, "
-                       "obs_sol_ss FROM [Redegeo-ant].[dbo].[tblSS] as s "
-                       "inner join tblServicos as v on s.cod_ser = v.cod_serv "
-                       "inner join tblTiposServico as t on s.cod_tp_ss = t.cod_tp_serv where matricula_ss = '" + request.form['matricula'] + "'")
+                       "logradouro_ss, "
+                       "nmr_imov_ss, "
+                       "compl_end_ss, "
+                       "tel_solic_ss, "
+                       "cod_ss_origem, "
+                       "o.nome_user, "
+                       "u.nm_undd, "
+                       "obs_sol_ss "
+                       "FROM[Redegeo-ant].[dbo].[tblSS] as s "
+                       "left join tblServicos as v on s.cod_ser = v.cod_serv "
+                       "left join tblEquipes e on s.cod_eqp_ss = e.cod_eqp "
+                       "left join tblUnidades u on s.cod_und_sol = u.cod_undd "
+                       "left join tblUsuarios o on s.cod_user_ss = o.cod_user "
+                       "where id_ss = " + request.form['servico'] )
+
         return render_template('servico.html', titulo='Consulta SSs do Redegeo', servicos=cursor)
     else:
-        flash('Digite o número de uma Matrícula')
+        flash('Digite o número da SS')
         return redirect(url_for('servico'))
 
 # from views import *
